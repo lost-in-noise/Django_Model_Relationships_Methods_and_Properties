d09f51fb5f5c8dfa706132c15e26296df216ce10
lost-in-noise <nancykikvadze@gmail.com>
2024-10-09 02:50:52 +0400

    Added .gitignore, requirements.txt, media and static setup

--- myproject/library/admin.py
+++ myproject/library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author, Book, Student, Course
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Student)
+admin.site.register(Course)
