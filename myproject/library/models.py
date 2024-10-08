a1de1e6b7ea8b805318e4dec76d9e2edcb4bf398
lost-in-noise <nancykikvadze@gmail.com>
2024-10-09 02:59:16 +0400

    Added verbose_name, verbose_name_plural, and Meta class to models

--- myproject/library/models.py
+++ myproject/library/models.py
@@ -3,32 +3,55 @@ from datetime import date
 
 # Task 1: Author and Book Models with ForeignKey Relationship
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Author Name")
+    
+    class Meta:
+        verbose_name = "Author"
+        verbose_name_plural = "Authors"
+        ordering = ['name']  # Orders authors alphabetically by name
     
     def __str__(self):
         return self.name
     
     def book_count(self):
-        return self.book_set.count()  # Return the number of books
+        return self.book_set.count()  # Return the number of books associated with this author
+
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, verbose_name="Book Title")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, verbose_name="Author")
+
+    class Meta:
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+        ordering = ['title']  # Orders books alphabetically by title
 
     def __str__(self):
         return self.title
 
+
 # Task 2: Student and Course Models with ManyToManyField
 class Course(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name="Course Name")
+
+    class Meta:
+        verbose_name = "Course"
+        verbose_name_plural = "Courses"
+        ordering = ['name']  # Orders courses alphabetically by name
 
     def __str__(self):
         return self.name
 
+
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    courses = models.ManyToManyField(Course)
-    date_of_birth = models.DateField(null=True, blank=True)  # For Task 3
+    name = models.CharField(max_length=100, verbose_name="Student Name")
+    courses = models.ManyToManyField(Course, verbose_name="Courses Enrolled")
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name="Date of Birth")  # For Task 3
+
+    class Meta:
+        verbose_name = "Student"
+        verbose_name_plural = "Students"
+        ordering = ['name']  # Orders students alphabetically by name
 
     def __str__(self):
         return self.name
