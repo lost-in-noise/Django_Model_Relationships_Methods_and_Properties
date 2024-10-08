8fa63bd462a69daf0e6fb6ff44fa28073e5f762f
lost-in-noise <nancykikvadze@gmail.com>
2024-10-09 02:33:23 +0400

    Initial commit for Django project with model relationships

---
+++ myproject/library/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from datetime import date
+
+# Task 1: Author and Book Models with ForeignKey Relationship
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    def book_count(self):
+        return self.book_set.count()  # Return the number of books
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# Task 2: Student and Course Models with ManyToManyField
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    courses = models.ManyToManyField(Course)
+    date_of_birth = models.DateField(null=True, blank=True)  # For Task 3
+
+    def __str__(self):
+        return self.name
+
+    # Method to list all courses
+    def list_courses(self):
+        return ", ".join([course.name for course in self.courses.all()])
+
+    # Task 3: Property to calculate age
+    @property
+    def age(self):
+        if self.date_of_birth:
+            today = date.today()
+            return today.year - self.date_of_birth.year - (
+                (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day)
+            )
+        return None
